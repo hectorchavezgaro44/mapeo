3a83aa10f3892a71851bebb16a388b9a01064b15
hectorchavezgaro44 <hector.chaga94@gmail.com>
2021-02-25 22:08:18 -0600

    puntos samuel

---
+++ puntos_aleatorios.R
@@ -0,0 +1,22 @@
+require(pacman)
+p_load(sf, tidyverse, hexbin, geogrid, here)
+
+
+colo <- st_read(here("muni_2018gw",  "muni_2018gw.shp" ), stringsAsFactors = FALSE, quiet = TRUE) %>%
+  st_transform(4326) 
+
+
+col <- filter(colo, NOM_ENT=="Ciudad de México") %>% 
+       mutate(puntos=1012)
+
+col <-  st_sample(col, size=col$puntos) %>% # random points, as a list ...
+  st_sf() 
+
+col <- col %>% 
+mutate(centroide=st_centroid(geometry))
+
+
+nom <- as.data.frame(sf::st_coordinates(col$centroide))
+
+
+save(nom, file=here("municipios", "puntos.rda"))
